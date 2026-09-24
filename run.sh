18d0135edd50261ee3b015f6ac617c8bb8fa6307
tavis <tavis@mostlyirrational.com>
2026-09-24 16:11:26 -0600

    Move .go files to cmd/postalboard directory

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 RUNNING=1
 while [ $RUNNING -eq 1 ]
 do
-    go build . || break
+    [ -e ./postalboard ] || (echo "Buildign..." && go build ./...) || break
     ./postalboard
     STATUS=$?
     if [ $STATUS -ne 2 ]
